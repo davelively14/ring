b81f0598911098cc0fe6d78b610515210a8e3933
Dave Lively <davelively14@gmail.com>
2017-11-21 19:38:57 -0500

    Added link_processes

--- lib/ring.ex
+++ lib/ring.ex
@@ -12,4 +12,20 @@ defmodule Ring do
         1/0
     end
   end
+
+  def link_processes(procs) do
+    link_processes(procs, [])
+  end
+
+  def link_processes([proc_1, proc_2|rest], linked_processes) do
+    send(proc_1, {:link, proc_2})
+    link_processes([proc_2|rest], [proc_1|linked_processes])
+  end
+
+  # Following exactly, but since this is the short circuit, this should be first
+  def link_processes([proc|[]], linked_processes) do
+    first_process = linked_processes |> List.last
+    send(proc, {:link, first_process})
+    :ok
+  end
 end
